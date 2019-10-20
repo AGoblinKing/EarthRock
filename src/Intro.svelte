041da4e762e988c132e0fb1c6787bab64030220c
AGoblinKing <AGoblinKing@isekai.dev>
2019-10-20 01:55:51 -0700

    no more anime

--- src/Intro.svelte
+++ src/Intro.svelte
@@ -1,27 +1,10 @@
 <script>
-import anime from "animejs/lib/anime.es.js"
 import Popup from "./Popup.svelte"
 import Tiles from "./Tiles.svelte"
 
 const clicked = () => {
     alert("Woah there speedy aint got nothing more yet")
 }
-
-anime({
-  targets: '.title',
-  points: [
-    { value: [
-      '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
-      '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
-    },
-    { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
-    { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
-    { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
-  ],
-  easing: 'easeOutQuad',
-  duration: 2000,
-  loop: true
-});
 </script>
 
 <style>
